5d4a1192d161ace4ac0a722618494a050cdf427f
Michael Seger <michael@net128.com>
2023-12-10 18:47:12 +0100

    fixed compliance tests

--- tools/format.sh
+++ tools/format.sh
@@ -3,7 +3,7 @@
 (printf "Benchmark\tn\tjp-java\tjp-graal\tjp-go\tjp-rust\tjp-js\n"
 cat | jq -rs '.[] | "|`\(.name)`|\(.n)|" + ([.time[] | min |
 	(.*1000|round)? // "N/A"] | min as $total_min | map(if . ==
-	$total_min then "*\(.)" else "\(.)" end) | join("|"))' | \
+	$total_min then "\(.) *" else "\(.)" end) | join("|"))' | \
 	tr -d '\`'|\
 	tr "|" "\t" | tr -s "\t" |\
 	sed -e 's/^[ \t]*//'
@@ -18,6 +18,8 @@ cat /tmp/2 |sed -e 's/^[ \t]*//;s/\t/|\t/g;s/.*/&|/;s/Bench.*/&\n-->&/' | \
         	s/|/ | /g;s/`//;s/`$//;
         	s/\([:-]\)`|/-\1|/g;
         	s/`-/--/g;s/-:|/--|/;
-		s/\*+([0-9]*)\*+/\$\${\\color{green}\1}$$/g;
-        	'|\
-        sed -E 's/( *)\*+([0-9]*)\**( *)/\$\${\\color[RGB]{121,189,66}\1\2\3}$$/g;'
+        	s/: |/--|/;s/| -/|--/g;s/: |/-:|/g;
+        	s/ *\*/ */g;s/-*$//;
+        	'
+        	#|\
+        #sed -E 's/( *)\*+([0-9]*)\**( *)/\$\${\\color[RGB]{121,189,66}\1\2\3}$$/g;'
