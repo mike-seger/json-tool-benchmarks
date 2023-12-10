2fe2abfafbe55fb072fded313fc3709292f781a6
Michael Seger <michael@net128.com>
2023-12-10 17:27:28 +0100

    fixed compliance tests

--- tools/jmespath/run-compliance.sh
+++ tools/jmespath/run-compliance.sh
@@ -18,6 +18,7 @@ function showSum() {
 	local name=$2
 	local first=1
 	if [[ "$prevname" != "" && "$prevname" != "$name" ]] ; then
+		echo "" >&2
 		echo -n "{\"name\": \"$prevname\", \"n\": $n, \"time\": {"
 		for tool in "${tools[@]}"; do
 			[ $first -ne 1 ] && echo -n ', '
@@ -31,50 +32,44 @@ function showSum() {
 		for tool in "${tools[@]}"; do
 			toolsum["$tool"]=0
 		done
+		echo "" >&2
 	fi
 }
 
-first=1
+lineno=0 
 while read line; do
-	IFS=$'\t'
-#	cols=($line)
-	if [[ $first == 1 ]]; then
-		first=0
+	#IFS=$'\t'
+	lineno=$(($lineno + 1))
+	if [[ $lineno == 1 ]]; then
 		pointers=($line)
 		continue
 	fi
 	
 	for i in {0..3}; do 
 		value=$(echo "$line" | cut -f$((i+1)))
-#echo "Key: "${pointers[$i]}" " >&2
 		printf -v "${pointers[$i]}" "%s" "$value"; 
 	done
 
-#	[[ "$name" != syntax ]] && continue
-
-#expression=$(echo "$expression"|sed -e "s/\*/\\*/g;"|tr -d '`')
-#expression=$(echo "$expression"|sed -e "s/\*/\\*/g;"|tr -d '`')
-	[[ "$name" == \#* ]] && continue
 	showSum "$prevname" "$name"
+	prevname=$name
+	[[ "$name" == \#* ]] && continue
+#	[[ "$name" != "syntax" ]] && continue
 
-	echo "$name ($n) -> $expression" >&2
+#	echo "$name ($n) -> $expression" >&2
+	echo -n . >&2
 	for tool in "${tools[@]}"; do
 		output=$(echo "$input" | bin/$tool $(echo "$expression") $n  2>/dev/null)
 		if [ $? != 0 ]; then
-			continue
+			echo >&2
+			echo "Failed ($tool) (line $lineno): $name ($n) -> $expression" >&2
+			break
 		fi
 		t=$(echo "$output" | tail -1)
 		current=${toolsum["$tool"]}
-#echo "$current" >&2
-#continue
-#echo "$current + 0$t"
 		toolsum["$tool"]=$(echo "$current + 0$t" | bc)
 	done
-	prevname=$name
-#	echo "prevname $prevname"
 done <<<$(cat tools/jmespath/tests-compliance.json |jq -r '
 	(map(keys) | add | unique) as $cols | map(. as $row | $cols | 
 	map($row[.])) as $rows | $cols, $rows[] | @tsv')
 
-#echo Hello "$prevname" ""
 showSum "$prevname" ""
